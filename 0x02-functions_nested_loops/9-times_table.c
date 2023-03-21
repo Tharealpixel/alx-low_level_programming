08563483631e2f2586cad9f927b27a0c75c59215
Tharealpixel <medboutahor@gamil.com>
2023-03-21 12:11:44 -0700

    d

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,16 +15,23 @@ void times_table(void)
 		res = i * j;
 
 		if (res > 9)
+		{
 			_putchar(' ');
 			_putchar('0' + res / 10);
+		}
 		else if (j != 0)
+		{
 			_putchar(' ');
 		_putchar('0' + res % 10);
-
+		}
 		if (j != 9)
+		{
 			_putchar(',');
+		}
 		else
+		{
 			_putchar('\n');
+		}
 	}
 	}
 }
