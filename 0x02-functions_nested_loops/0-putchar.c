cee07c48739f01abe60819458357bf97aa070948
Tharealpixel <medboutahor@gamil.com>
2023-03-21 03:47:51 -0700

    wow

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+/*Main functoin 
+ *
+ * Returns zero
+ */
 int main(void) 
-{
-/*
+{/*
 * prints _putchar and returs to new line
 */
 printf("_putchar\n");
-return (0);
-}
+return (0);}
