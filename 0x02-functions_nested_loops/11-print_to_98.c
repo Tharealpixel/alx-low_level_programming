0252c794fc8a942fe09f557b415ded51efaf220f
Tharealpixel <medboutahor@gamil.com>
2023-03-21 12:45:55 -0700

    d

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * print natural numbers from 0 to 98
+ *
+ * Numbers kept  separated by a comma, followed by a space
+ *
+ */
 
 void print_to_98(int n)
 {
