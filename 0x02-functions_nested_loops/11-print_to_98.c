4feadcf8e991e3df2a3bbf82b4b23caba4407bbd
Tharealpixel <medboutahor@gamil.com>
2023-03-21 13:13:35 -0700

    d

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,14 +17,14 @@ void print_to_98(int n)
 	else if (n < 98)
 	{
 	
-		for (n < 98; n++;)
+		for (int i = n; i <= 98; i++)
 		{
 			printf(" %d",n);
 		}
 	}
 	else
 	{
-		for (n > 98; n--;)
+		for (int i = n; i >= 98; i--)
 		{
 			printf(" %d",n);
 		}
