d88b483bbc666b4a8d8aff5b57bee72e6c5df9b6
Tharealpixel <medboutahor@gamil.com>
2023-03-21 05:04:57 -0700

    wow

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,17 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter = 'a';
-    int i;
+	char letter = 'a';
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        letter = 'a';
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		letter = 'a';
+		_putchar('\n');
+	}
 }
