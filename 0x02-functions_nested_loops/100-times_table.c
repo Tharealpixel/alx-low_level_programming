723bef606311244b732efa01ce0e4b7557b5db76
Tharealpixel <medboutahor@gamil.com>
2023-03-21 13:40:33 -0700

    g

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include"main.h"
+
+/**
+ * print_times_table - prints time table of n
+ *
+ * @n: takes number input
+*/
+
+void print_times_table(int n)
+{
+	int i, j, num;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; ++num)
+		{
+			_putchar(48);
+			for (j = 1; mult <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				i = num * j;
+
+				if (i <= 9)
+					_putchar(' ');
+				if (i <= 99)
+					_putchar(' ');
+
+				if (i >= 100)
+				{
+					_putchar((i / 100) + 48);
+					_putchar((i / 10) % 10 + 48);
+				} else if (i <= 99 && i >= 10)
+					_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
