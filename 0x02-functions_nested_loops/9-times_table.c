70f625be19790e49de2c1a9cd61c8d1938e7908c
Tharealpixel <medboutahor@gamil.com>
2023-03-21 12:28:51 -0700

    d

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,28 +12,27 @@
 
 void times_table(void)
 {
-    int i, j, res;
+	int i, j, res;
 
-    for (i = 0; i <= 9; ++i)
-    {
-        printf("%d", 0);
-        for (j = 1; j <= 9; ++j)
-        {
-            printf(", ");
-            res = i * j;
+	for (i = 0; i <= 9; ++i)
+	{
+		printf("%d", 0);
+		for (j = 1; j <= 9; ++j)
+		{
+			printf(", ");
+			res = i * j;
 
-            /*
-             * put space if product is a single number
-             * place the first digit if its two numbers
-            */
-            if (res <= 9)
-                printf(" ");
-            else
-                printf("%d", res / 10);
+			/*
+			* put space if product is a single number
+			* place the first digit if its two numbe
+			**/
+			if (res <= 9)
+				printf(" ");
+			else
+				printf("%d", res / 10);
 
-            printf("%d", res % 10);
-        }
-        printf("\n");
-    }
+			printf("%d", res % 10);
+		}
+		printf("\n");
+	}
 }
-
