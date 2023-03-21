9aefca1ce1deedc94a17385f6f8829c7a6943c2a
Tharealpixel <medboutahor@gamil.com>
2023-03-21 06:45:40 -0700

    commit to onething in life

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,31 @@
 #include "main.h"
 
 /**
+ * _islower - checks if a character is lowercase
+ *
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+        if (c >= 'a' && c <= 'z')
+        {
+                return 1;
+        }
+        else
+        {
+                return 0;
+        }
+}
+
+/**
  * main - check the code.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int _islower(int c);
-	{
-		if (c >= 'a' && c<= 'z')
-		{
-			return 1;
-		} 	else 
-		{
-			return 0;
-		}
-	}
-	
+	/* my code */
+	return 0;
+}
