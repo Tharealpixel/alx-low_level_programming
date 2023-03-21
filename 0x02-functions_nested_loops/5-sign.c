4c8cf844235b5168080783aeb817e7dbdce0edb0
Tharealpixel <medboutahor@gamil.com>
2023-03-21 07:38:06 -0700

    mom

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number.
- * n: The number to check.
+ *@ n: The number to check.
  *
  * Return: 1 if n is positive, -1 if n is negative, 0 otherwise.
  */
