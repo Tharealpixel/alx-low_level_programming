75636f25910a899ec2a4f1fbba119d008708e03b
Tharealpixel <medboutahor@gamil.com>
2023-03-21 13:26:11 -0700

    g

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,15 +17,15 @@ void print_to_98(int n)
 	
 		for (i = n; i < 98; i++)
 		{
-			printf("%d, ",n);
+			printf("%d, ",i);
 		}
 	}
 	else
 	{
-		for (i = n; i > 98; --i)
+		for (i = n; i > 98; i--)
 		{
-			printf("%d, ",n);
+			printf("%d, ",i);
 		}
-		printf("\n");
+		printf("98\n");
 	}
 }
