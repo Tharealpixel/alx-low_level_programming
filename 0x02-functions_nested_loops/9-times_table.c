f8e20d88525a638e6eef4094ac6739a57d0a7b20
Tharealpixel <medboutahor@gamil.com>
2023-03-21 12:03:25 -0700

    d

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,22 +8,22 @@ void times_table(void)
 {
 	int i, j, res;
 
-    for (i = 0; i <= 9; i++)
-    {
-	    for (j = 0; j <= 9; j++)
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
 	{
 		res = i * j;
 
-	    if (res > 9)
-		    _putchar('0' + res / 10);
-	    else if (j != 0)
-		    _putchar(' ');
-	    _putchar('0' + res % 10);
+		if (res > 9)
+			_putchar('0' + res / 10);
+		else if (j != 0)
+			_putchar(' ');
+		_putchar('0' + res % 10);
 
-	    if (j != 9)
-		    _putchar(',');
-	    else
-		    _putchar('\n');
+		if (j != 9)
+			_putchar(',');
+		else
+			_putchar('\n');
 	}
-    }
+	}
 }
