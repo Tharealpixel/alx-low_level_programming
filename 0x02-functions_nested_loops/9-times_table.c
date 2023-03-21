85cf5a9cd8dc9744501b3b2060f9617af3281794
Tharealpixel <medboutahor@gamil.com>
2023-03-21 11:59:23 -0700

    d

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+
+void times_table(void)
+{
+	int i, j, res;
+
+    for (i = 0; i <= 9; i++)
+    {
+	    for (j = 0; j <= 9; j++)
+	{
+		res = i * j;
+
+	    if (res > 9)
+		    _putchar('0' + res / 10);
+	    else if (j != 0)
+		    _putchar(' ');
+	    _putchar('0' + res % 10);
+
+	    if (j != 9)
+		    _putchar(',');
+	    else
+		    _putchar('\n');
+	}
+    }
+}
