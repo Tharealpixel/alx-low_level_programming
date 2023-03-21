4d660e2d613172e0ff1d9db2d8efc947af2158c5
Tharealpixel <medboutahor@gamil.com>
2023-03-21 07:21:10 -0700

    zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/*
+ *
+ * tkherbi9 dilahom
+ *
+ *
+ * walo mafhmna wlo
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n>0)
+	{
+		printf("+\n");
+		return(1);
+	}
+	if (n<0)
+	{
+		printf("-\n");
+		return(-1);
+	}
+	if (n=0)
+	{
+		printf("0\n");
+		return(0);
+	}
+}
