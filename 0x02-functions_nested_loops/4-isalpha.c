ac266181a08703ad6fc160b23d41b7e96d3cde9e
Tharealpixel <medboutahor@gamil.com>
2023-03-21 07:03:22 -0700

    ff

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c);
+{
+	if (c<='A' && c>= 'Z' && c<='a' && c>='z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
