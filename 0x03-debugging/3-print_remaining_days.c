e787478d5f37f0dba357abd2688245d398d29542
Tharealpixel <medboutahor@gamil.com>
2023-03-22 11:41:00 -0700

    heelo

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	/**
 	 * leap year when it can be divided
-	 * by 4 evenly or by 100 and 400
+	 * by 4 evenly or by 100 or 400
 	*/
 	if ((year % 4 == 0) || (year % 100 == 0 && year % 400 == 0))
 	{
