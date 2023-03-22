0f7705ab6f11f65b4cbb80cd7c104c2c480d2c34
Tharealpixel <medboutahor@gamil.com>
2023-03-22 03:54:11 -0700

    fugs

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		/**
+		 * @i:does not increment so the loop will never end.
+		 *
+		 */
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
