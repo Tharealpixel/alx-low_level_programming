ffce02121adbea828f558fe7e803efa3bf2aa9a1
Tharealpixel <medboutahor@gamil.com>
2023-03-23 10:04:19 -0700

    wow

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: The number of the character _ 
+ * should be printed
+ */
+
 void print_line(int n)
 {
 	int i;
