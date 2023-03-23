2d36dc2ca82f828348eecd9f432ed732c4bc179c
Tharealpixel <medboutahor@gamil.com>
2023-03-23 10:10:50 -0700

    wow

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,9 +11,13 @@ void print_line(int n)
 {
 	int i;
 
-	for(i = 0;i < n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		_putchar(95);
-	}
+		for(i = 0;i < n; i++)
+		{
+			_putchar(95);
+		}
 	_putchar('\n');
 }
