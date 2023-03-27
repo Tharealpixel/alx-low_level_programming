ba5befa6f35818c771af119f4f589ffe453fe2ab
Tharealpixel <medboutahor@gamil.com>
2023-03-27 13:50:31 -0700

    advanced

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASS_LEN 12
+
+int main(void)
+{
+    srand(time(NULL));
+    char pass[PASS_LEN + 1];
+
+    for (int i = 0; i < PASS_LEN; i++) {
+        int r = rand() % 62;
+        if (r < 26) {
+            pass[i] = 'a' + r;
+        } else if (r < 52) {
+            pass[i] = 'A' + r - 26;
+        } else {
+            pass[i] = '0' + r - 52;
+        }
+    }
+    pass[PASS_LEN] = '\0';
+
+    printf("%s", pass);
+
+    return 0;
+}
+
