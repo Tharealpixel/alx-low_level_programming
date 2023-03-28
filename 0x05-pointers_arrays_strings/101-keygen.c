1474ddc8ef83cc191a0ed5ade51369d76646abd8
Tharealpixel <medboutahor@gamil.com>
2023-03-28 15:59:08 -0700

    lol

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,20 @@ int main(void)
 {
     srand(time(NULL));
 
-    char password[PASSWORD_LENGTH + 1] = {0};
-    char valid_characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+1234567890";
+    char password[PASSWORD_LENGTH + 1];
+    char valid_characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    for (int i = 0; i < PASSWORD_LENGTH; i++)
+    int i;
+    for (i = 0; i < PASSWORD_LENGTH; i++)
     {
         int random_index = rand() % (sizeof(valid_characters) - 1);
         password[i] = valid_characters[random_index];
     }
 
+    password[i] = '\0';  // add null terminator at the end
+
     printf("%s", password);
 
     return 0;
 }
+
