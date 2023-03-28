fc9e5dba687b9ab23277044340946e056d1f9345
Tharealpixel <medboutahor@gamil.com>
2023-03-28 15:33:47 -0700

    w22

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,23 +10,27 @@
 
 int _atoi(char *s)
 {
-	unsigned int num = 0;
-	int sign = 1;
+    unsigned int num = 0;
+    int sign = 1;
+    int i = 0;
 
-	do {
-		if (*s == '-')
-		{
-			sign *= -1;
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			num = (num * 10) + (*s - '0');
-		}
-		else if (num > 0)
-		{
-			break;
-		}
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-')
+        {
+            sign *= -1;
+        }
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            num = (num * 10) + (s[i] - '0');
+        }
+        else if (num > 0)
+        {
+            break;
+        }
+        i++;
+    }
 
-	}
-	return (num * sign);
+    return (num * sign);
 }
+
