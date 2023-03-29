8c025bd3ebc74d0fee7ce5ef9862a61edc6f742d
Tharealpixel <medboutahor@gamil.com>
2023-03-29 08:44:50 -0700

    hhhhh

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*temp != '\0')
+	{
+		temp++;
+	}
+
+	while (*src != '\0')
+	{
+		*temp++ = *src++;
+	}
+
+	*temp = '\0';
+
+	return dest;
+}
