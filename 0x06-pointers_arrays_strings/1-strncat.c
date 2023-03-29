0b3f24b8196036144d84d9cb1bf6baf76c24e024
Tharealpixel <medboutahor@gamil.com>
2023-03-29 08:59:54 -0700

    hhhhh

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,23 +13,23 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int i, j;
+	int i, j;
 
-        i = 0;
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-        j = 0;
-        while (j < n && src[j] != '\0')
-        {
-                dest[i] = src[j];
-                i++;
-                j++;
-        }
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-        dest[i] = '\0';
+	dest[i] = '\0';
 
-        return (dest);
+	return (dest);
 }
