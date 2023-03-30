1e4d087a0c6c5d251824e02cff8186b18846fab3
Tharealpixel <medboutahor@gamil.com>
2023-03-30 07:28:42 -0700

    wow

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,26 +4,26 @@
 int main(void)
 {
 	int num;
-	int arr[5];
-	int *ptr;
+	int a[5];
+	int *p;
 
-	arr[2] = 1024;
-	ptr = &num;
+	a[2] = 1024;
+	p = &num;
   
 	/*
 	* Write a line of code that will assign the value 98 to the integer
 	* variable located 5 integers after the memory address pointed to by ptr.
 	* 
 	* Restictions:
-	* - Do not use the array arr
-	* - Do not modify the pointer ptr
+	* - Do not use the array a
+	* - Do not modify the pointer p
 	* - Use only one statement
 	*/
 
-	*(ptr + 5) = 98; /* Assign 98 to the integer variable located 5 integers after ptr*/
+	*(p + 5) = 98; /* Assign 98 to the integer variable located 5 integers after p*/
 
 	/* This should print "98\n" */
-	printf("*(ptr + 5) = %d\n", *(ptr + 5));
+	printf("*(p + 5) = %d\n", *(p + 5));
 
 	return (0);
 }
