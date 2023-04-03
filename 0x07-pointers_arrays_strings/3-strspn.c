8b287719db1b5197fcf104dce56dc7f8d3c01c82
Tharealpixel <medboutahor@gamil.com>
2023-04-03 07:59:21 -0700

    trying this bit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n;
+	while(*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{	
+			if(*s == accept[i])
+			{
+				n++;
+			}
+		}
+	}
+}
