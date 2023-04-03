d1efe1887beb1f5b5078136b1c8edcf717111ad5
Tharealpixel <125474593+Tharealpixel@users.noreply.github.com>
2023-04-03 11:32:16 +0000

    f

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		for (num = 0; num <= n; ++num)
+		for (num = 0; num <= n; num++)
 		{
 			_putchar(48);
 			for (j = 1; j <= n; j++)
@@ -23,16 +23,23 @@ void print_times_table(int n)
 				i = num * j;
 
 				if (i <= 9)
+				{
 					_putchar(' ');
+				}
 				if (i <= 99)
+				{
 					_putchar(' ');
+				}
 
 				if (i >= 100)
 				{
 					_putchar((i / 100) + 48);
 					_putchar((i / 10) % 10 + 48);
-				} else if (i <= 99 && i >= 10)
+				}
+				else if (i <= 99 && i >= 10)
+				{
 					_putchar((i / 10) + 48);
+				}
 				_putchar((i % 10) + 48);
 			}
 			_putchar('\n');
