9bfd861ddf7ef0e6865a904a79d76b496b5bb786
Tharealpixel <125474593+Tharealpixel@users.noreply.github.com>
2023-04-03 11:52:26 +0000

    l

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * numLength - returns the length of string
  *
@@ -18,7 +19,7 @@ int numLength(int num)
 	while (num)
 	{
 		num = num / 10;
-		length += 1;
+		length = length + 1;
 	}
 
 	return (length);
@@ -30,43 +31,45 @@ int numLength(int num)
  * Description: prints the first 98 Fibonacci numbers
  *	starting with 1 and 2 followed by a new line
  *
- *	Solution was copied from Nobert Patrick
- *	Wise, github handle: Trikcode
- *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	int count, initial0s;
-	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+	int count, initials;
+	unsigned long f1 = 1, f2 = 2, sum, max = 100000000, f1a = 0, f2a = 0, suma = 0;
 
-	for (count = 1; count <= 98; ++count)
+	for (cont = 1; cont <= 98; cont++)
 	{
-		if (f1o > 0)
-			printf("%lu", f1o);
-		initial0s = numLength(mx) - 1 - numLength(f1);
+		if (f1a > 0)
+		{
+			printf("%lu", f1a);
+		}
+		initials = numLength(max) - 1 - numLength(f1);
 
-		while (f1o > 0 && initial0s > 0)
+		while (f1a > 0 && initials > 0)
 		{
 			printf("%d", 0);
-			--initial0s;
+			initials--;
 		}
 
 		printf("%lu", f1);
 
-		sum = (f1 + f2) % mx;
-		sumo = f1o + f2o + (f1 + f2) / mx;
+		sum = (f1 + f2) % max;
+		sumo = f1a + f2a + (f1 + f2) / mx;
 		f1 = f2;
-		f1o = f2o;
+		f1a = f2a;
 		f2 = sum;
-		f2o = sumo;
+		f2o = suma;
 
-		if (count != 98)
+		if (cont != 98)
+		{
 			printf(", ");
+		}
 		else
+		{
 			printf("\n");
+		}
 	}
-
 	return (0);
 }
