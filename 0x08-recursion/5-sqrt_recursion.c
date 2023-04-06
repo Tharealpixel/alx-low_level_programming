0ecdc37aeff6677075fbc418abb9cc73cebdeaca
Tharealpixel <medboutahor@gamil.com>
2023-04-05 17:31:54 -0700

    betty wasnt gentle :)

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,21 +5,22 @@
  * @n: The number to find the square root of.
  * @i: The current integer value to check.
  *
- * Return: The square root of n, or -1 if n does not have a natural square root.
+ * Return: The square root of n,
+ *  or -1 if n does not have a natural square root
  */
-int find_sqrt(int n,int i)
+int find_sqrt(int n, int i)
 {
-	if (i*i != n && i <= n / 2)
+	if (i * i != n && i <= n / 2)
 	{
-		i = find_sqrt (n, i + 1);
+		i = find_sqrt(n, i + 1);
 	}
-	if (i*i == n)
+	if (i * i == n)
 	{
-		return i;
+		return (i);
 	}
 	else
 	{
-		return -1;
+		return (-1);
 	}
 }
 
@@ -27,21 +28,22 @@ int find_sqrt(int n,int i)
  * _sqrt_recursion - Returns the square root of a number.
  * @n: The number to find the square root of.
  *
- * Return: The square root of n, or -1 if n does not have a natural square root.
+ * Return: The square root of n,
+ * or -1 if n does not have a natural square root.
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	else if (n == 0)
 	{
-		return 0;
+		return (0);
 	}
 	else if (n == 1)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
