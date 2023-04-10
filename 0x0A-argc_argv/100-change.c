d2b8968b2793b15e684a2e54ef11d32a6cb6f690
Tharealpixel <medboutahor@gamil.com>
2023-04-10 14:14:29 -0700

    d

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,28 +2,17 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins
- *        to make change for an amount of money.
+ * coin_counter - counts the minimum number of coins needed to represent
+ *                a given amount of money in cents.
  *
- * @argc: count
- * @argv: array
+ * @cents: the amount of money in cents
+ * @coins: a variable to hold the total number of coins counted
  *
- * Return: 0 if one arg, else 1
-*/
-int main(int argc, char *argv[])
+ * Return: the minimum number of coins.
+ */
+int coin_counter(int cents, int coins)
 {
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
-	{
-		int cents;
-		int coins = 0;
-
-		cents = atoi(argv[1]);
-		if (cents < 0)
+	if (cents < 0)
 		{
 		printf("0\n");
 		return (0);
@@ -52,6 +41,31 @@ int main(int argc, char *argv[])
 		{
 			coins += 1;
 		}
+		return (coins);
+}
+/**
+ * main - prints the minimum number of coins
+ *        to make change for an amount of money.
+ *
+ * @argc: count
+ * @argv: array
+ *
+ * Return: 0 if one arg, else 1
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int cents;
+		int coins = 0;
+
+		cents = atoi(argv[1]);
+		coins = coin_counter(cents, coins);
 		printf("%d\n", coins);
 	}
 	return (0);
