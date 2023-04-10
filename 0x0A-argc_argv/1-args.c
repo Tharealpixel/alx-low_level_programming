5b23d900f3cec328b04132aa298b17dbfa823ba2
Tharealpixel <medboutahor@gamil.com>
2023-04-10 08:04:30 -0700

    my life is in ruins

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the number of arguments passed into it.
+ * @argc: Number of command line arguments
+ * @argv: Array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		while(*argv)
+		{
+			i++;
+			argv++;
+		}
+		printf("%d\n", i - 1);
+	}
+	return (0);
+}
