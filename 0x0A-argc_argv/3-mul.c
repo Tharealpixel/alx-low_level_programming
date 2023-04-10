4b105c9dd3a2ccae1ba4390352a4ded13a1c2d01
Tharealpixel <medboutahor@gamil.com>
2023-04-10 08:25:34 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int mult;
+
+	if (argc == 1)
+		printf("Error");
+	else
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+	}
+	return (0);
+}
