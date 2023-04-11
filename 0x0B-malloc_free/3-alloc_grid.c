e095d11fa732a6a84e5dfe10e6d60c6250d119ab
Tharealpixel <medboutahor@gamil.com>
2023-04-11 14:59:49 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,17 +8,33 @@
  *
  * Return: a pointer to the allocated 2D array, or NULL on failure
  */
-int *alloc_grid(int width, int height)
+int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **grid;
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	int *grd = malloc(sizeof(int) * width * height);
-	if (grd == NULL)
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
 		return (NULL);
 
-	for (int i = 0; i < width * height; i++)
-		grd[i] = 0;
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
 
-	return (grd);
+			free(grid);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
 }
