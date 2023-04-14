725ac68664d82321e75149859dc850501082594e
Tharealpixel <medboutahor@gamil.com>
2023-04-13 17:40:17 -0700

    lol

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
+	ptr  = malloc(size * sizeof(int));
+
+	if (ptr == 0)
+		return (NULL);
+
+	memset(ptr, 0, nmemb * size);
+	return (ptr);
+
+}
