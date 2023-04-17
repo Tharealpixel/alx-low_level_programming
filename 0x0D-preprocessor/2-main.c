873f21f0cf5301f6604611137b812fb6866db0c8
Tharealpixel <medboutahor@gamil.com>
2023-04-17 14:13:53 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - function
  *
  * Description: prints the name of the file it was
- *              compiled from, followed by a new line
- *
- * Return: void
+ *		compiled from, followed by a new line
+ * Return: 0
 */
-void main(void)
+
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
