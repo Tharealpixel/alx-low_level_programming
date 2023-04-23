b960355d72ff180c4efc537d16f5210f13fc3bf7
Tharealpixel <medboutahor@gamil.com>
2023-04-23 06:55:26 -0700

    well

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,7 +8,11 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+/**
+ * struct printer - structure to selct right function
+ * @symb: sypbole to selct the datatype
+ * @print: A function pointer that prints the right data type
+ */
 typedef struct printer
 {
 	char *symb;
