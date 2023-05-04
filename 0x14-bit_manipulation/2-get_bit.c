9d2a7eaee8446f7420aed5ace94d1924c2f4041e
Tharealpixel <medboutahor@gamil.com>
2023-05-04 11:04:07 -0700

    W

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+/**
+ * get_bit - Gets the value of a bit in a  int
+ * @n: The numbre
+ * @index: the index of the bt to be returned
+ *
+ * Return: 1 or 0, or -1 if an error occurs.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return -1;
+
+	return (n >> index) & 1;
+}
