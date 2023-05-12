67f5e1fad3a3ff8671c39d9b73690212fcccc086
Tharealpixel <medboutahor@gmail.com>
2023-05-12 14:51:21 -0700

    W

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,18 +1,24 @@
 #include "main.h"
-
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ * @filename: ptr to filename
+ * @text_content: text to add
+ *
+ * Return: 1 on success and -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len, w;
 
 	if (filename == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
-		return -1;
+		return (-1);
 	}
 
 	if (text_content != NULL)
@@ -26,10 +32,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (w == -1)
 		{
 			close(fd);
-			return -1;
+			return (-1);
 		}
 	}
 
 	close(fd);
-	return 1;
+	return (1);
 }
