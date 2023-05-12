b884283fe1c0079a3f644f32d3894b23ad64ca1f
Tharealpixel <medboutahor@gmail.com>
2023-05-12 14:47:10 -0700

    W

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,12 +2,14 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, len, w;
+
 	if (filename == NULL)
 	{
 		return -1;
 	}
 
-	int fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
 		return -1;
@@ -15,12 +17,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		int len = 0;
+		len = 0;
 		while (text_content[len])
 		{
 			len++;
 		}
-		int w = write(fd, text_content, len);
+		w = write(fd, text_content, len);
 		if (w == -1)
 		{
 			close(fd);
