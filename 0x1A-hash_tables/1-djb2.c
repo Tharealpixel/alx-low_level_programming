5a28b3d61a13564501661c8bd08594b7df2fdcb1
Tharealpixel <medboutahor@gmail.com>
2023-07-07 22:10:10 +0100

    W

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int h;
+	int c;
+
+	h = 5381;
+	while ((c = *str++))
+	{
+		h = ((h << 5) + h) + c;
+	}
+	return (h);
+}
